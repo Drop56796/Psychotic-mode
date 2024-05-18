cc2e6f819128f71507ca6c9bd3fe277aa06fdac9
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-18 19:02:10 +0800

    Update 0.lua

--- 0.lua
+++ 0.lua
@@ -1,113 +1,27 @@
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Psychotic Mode V3",true)
-wait(5)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "loading"
+	cue2.SoundId = "rbxassetid://6657340839"
+	cue2.Volume = 99999999999
+	cue2.PlaybackSpeed =0.25
+	cue2:Play()
+
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Crazy Mode",true)
+wait(3.5)
 
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Make By Darkness and other person",true)
-wait(5)
+wait(3.5)
 
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("...",true)
-wait(5)
+wait(3.5)
 
 ---====== Load achievement giver ======---
 local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
 
 ---====== Display achievement ======---
 achievementGiver({
-    Title = "Psychotic mode V3 is exeute",
-    Desc = "What are you doing?",
-    Reason = "Good luck. Bad Hacker...",
-    Image = "rbxassetid://13905664807"
+    Title = "Bad Hacker exeute it",
+    Desc = "YOU ARE DIED",
+    Reason = "Crazy mode is exeute",
+    Image = "rbxassetid://12583460254"
 })
-
--- Custom Door Sounds and Floor2 Candle
-loadstring(game:HttpGet("https://pastebin.com/raw/iAhTGdBh"))()
-loadstring(game:HttpGet("https://pastebin.com/raw/6brG9uma"))()
-
--- Super A60
-coroutine.wrap(function()
-    while true do
-        wait(math.random(1000,1025))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/a60/main/60.lua"))()
-    end
-end)()
-
--- detph
-coroutine.wrap(function()
-    while true do
-        wait(math.random(890,910))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/detph-spawner/main/detph.lua"))()
-    end
-end)()
-
--- Crazy Hunger
-coroutine.wrap(function()
-    while true do
-        wait(math.random(900,915))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Crazy-Hunger/main/Hunger.lua"))()
-    end
-end)()
-
--- terrorist
-coroutine.wrap(function()
-    while true do
-        wait(math.random(999,1000))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Spawn-terrorist/main/Spawn.lua"))()
-    end
-end)()
-
--- G95
-coroutine.wrap(function()
-    while true do
-        wait(650)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/G95/main/G95.lua"))()
-    end
-end)()
-
--- Mangle
-coroutine.wrap(function()
-    while true do
-        wait(math.random(790,800))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/M/main/M.lua"))()
-    end
-end)()
-
--- Deeper
-coroutine.wrap(function()
-    while true do
-        wait(699)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/D/main/D.lua"))()
-    end
-end)()
-
--- A200
-coroutine.wrap(function()
-    while true do
-        wait(100)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/A200/main/200.lua"))()
-    end
-end)()
-
--- Traumatized
-coroutine.wrap(function()
-    while true do
-        wait(250)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Trumanet/main/Traumatizemd.lua"))()
-    end
-end)()
-
--- A666
-coroutine.wrap(function()
-    while true do
-        wait(700)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/A666/main/A666.lua"))()
-    end
-end)()

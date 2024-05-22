3e654ab5891cd2a9bc85b2ae13e6ac2ad7d76ec5
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-22 22:17:29 +0800

    Update monste and Door Sounds.lua

--- monste and Door Sounds.lua
+++ monste and Door Sounds.lua
@@ -1,5 +1,5 @@
 -- Custom Door Sounds
-loadstring(game:HttpGet("https://pastebin.com/raw/iAhTGdBh"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Doors-Sound/main/Doors.lua"))()
 loadstring(game:HttpGet("https://pastebin.com/raw/6brG9uma"))()
 
 -- Super A60

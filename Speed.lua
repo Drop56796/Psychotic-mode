169a6282a888bcf5a89f1a4314f4927dc259402d
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-26 08:08:03 +0800

    Update Speed.lua

--- Speed.lua
+++ Speed.lua
@@ -1,2 +1,2 @@
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 10
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Speed/main/Script.lua"))()

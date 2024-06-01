7720d29ce0f5d8e1d3a7d55dfdea73054dba837c
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-01 20:55:18 +0800

    Update 0.lua

--- 0.lua
+++ 0.lua
@@ -1,6 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Something/main/something.lua"))()
-wait(0.001)
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Doors-Sound/main/Doors.lua"))()
 loadstring(game:HttpGet("https://pastebin.com/raw/6brG9uma"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Something/main/something.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Psychotic-mode/main/monste%20and%20Door%20Sounds.lua"))()

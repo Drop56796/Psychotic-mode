b094c6cecf16cfd078b09c31ffe2fbb4018ecb9d
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-15 10:23:17 +0800

    Update 0.lua

--- 0.lua
+++ 0.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Jahani-john/mayhem-mode/main/mayhemmode-main/loader.lua'))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Doors-Sound/main/Doors.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Psychotic-mode/main/monste%20and%20Door%20Sounds.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Something/main/something.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Jahani-john/mayhem-mode/main/mayhemmode-main/loader.lua'))()
